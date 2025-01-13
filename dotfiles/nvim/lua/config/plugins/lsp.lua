7277ac4628ce881bc23f1a998568b363cde3dadd
Ivan Santiago <ivansantiago.junior@gmail.com>
2025-01-13 11:50:03 -0300

    add go to definition by lsp

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
 			vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 			vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
 			vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 			vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help)
 
 			-- formatting
