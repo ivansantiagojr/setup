fdbb2ff931a97c831c6bc290c0256293c5fa9e03
Ivan Santiago <ivansantiago.junior@gmail.com>
2025-01-18 22:20:29 -0300

    setup zond

---
+++ zond_config.lua
@@ -0,0 +1,23 @@
+return {
+	config = {
+		dotpath = 'dotfiles'
+	},
+	mappings = {
+		gitconfig = {
+			source = '.gitconfig',
+			destination = '~/.gitconfig'
+		},
+		nvim = {
+			source = 'nvim',
+			destination = '~/.config/nvim'
+		},
+		['clang-format'] = {
+			source = '.clang-format',
+			destination = '~/.clang-format'
+		},
+		tmux = {
+			source = '.tmux.conf',
+			destination = '~/.tmux.conf'
+		},
+	}
+}
