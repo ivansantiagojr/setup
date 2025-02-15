7ec2858497d5e1c466acdb71aee7967f1b659330
Ivan Santiago <ivansantiago.junior@gmail.com>
2025-02-15 01:54:23 -0300

    place undodir on home and set expandtab to true

--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -8,18 +8,18 @@ vim.o.errorbells = false -- ding ding!
 vim.o.wrap = true -- wrap long lines
 vim.o.swapfile = false -- don't need no swap files!
 vim.o.backup = false  -- ... or a backup!
---vim.o.undodir = "~/.nvim/undodir"  -- sets where undo files land
+vim.o.undodir = "~/.nvim/undodir"  -- sets where undo files land
 vim.o.scrolloff = 8  -- scroll offwidth so it's not the very bottom
 vim.o.colorcolumn = "80"  -- bar at 80 chars width
 vim.o.signcolumn = "auto:1" -- always set the far left bar/column
 -- vim.o.nocompatible = true
 vim.o.mouse = "a"  -- always enable mouse mode
 vim.o.tabstop = 4  -- Tabs are 4 spaces: Reference - https://stackoverflow.com/questions/1878974/redefine-tab-as-4-spaces
-vim.o.softtabstop = 0
 vim.o.shiftwidth = 4
-vim.o.expandtab = false
+vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.cursorline = true
+
 
 vim.o.ignorecase = true  -- ignore case when searching
 vim.o.smartcase = true  -- Unless we explicitly use cases in search
@@ -28,7 +28,7 @@ vim.o.splitright = true -- automagically v split going right
 vim.opt.listchars = {  -- see hidden chars and their colors
     tab = "| ",
     eol = '¬',
-    trail = '·'
+    trail = '·',
 }
 vim.o.list = true
 vim.o.updatetime = 100
