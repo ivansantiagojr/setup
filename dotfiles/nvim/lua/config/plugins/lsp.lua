b9e236c880b7893d1d3fd12bd6ea3e11e348db5d
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-02-25 19:24:43 -0300

    fix formatting

--- dotfiles/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/nvim/lua/config/plugins/lsp.lua
@@ -1,30 +1,30 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				opts = {
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-		},
-		config = function()
-			-- setups are in plugins/mason.lua
-			-- needed while not in neovim nightly
-			vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
-			vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
-			vim.keymap.set('n', 'grr', vim.lsp.buf.references)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-			vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help)
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            -- setups are in plugins/mason.lua
+            -- needed while not in neovim nightly
+            vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+            vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+            vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help)
 
-			-- formatting
-			vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
-		end,
-	}
+            -- formatting
+            vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
+        end,
+    }
 }
--- dotfiles/nvim/lua/config/plugins/mason.lua
+++ dotfiles/nvim/lua/config/plugins/mason.lua
@@ -1,19 +1,19 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "zls", "pyright", "clangd", "bashls" },
-			automatic_installation = true,
-		})
-		local capabilities = require('blink.cmp').get_lsp_capabilities()
-		require('mason-lspconfig').setup_handlers({
-			function(server_name)
-				require('lspconfig')[server_name].setup { capabilities = capabilities }
-			end,
-		})
-	end,
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "zls", "pyright", "clangd", "bashls" },
+            automatic_installation = true,
+        })
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        require('mason-lspconfig').setup_handlers({
+            function(server_name)
+                require('lspconfig')[server_name].setup { capabilities = capabilities }
+            end,
+        })
+    end,
 }
