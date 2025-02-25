60acdd8619a827337763f4e157f05fae8f243503
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-02-25 19:23:50 -0300

    jyba was moved to my user

--- dotfiles/nvim/lua/config/plugins/jyba.lua
+++ dotfiles/nvim/lua/config/plugins/jyba.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"brasilisclub/jyba",
-		dependencies = "nvim-lua/plenary.nvim",
-		config = function()
-			local jyba = require("jyba")
-			vim.keymap.set("n", "<C-m>", jyba.toggle_window)
-		end
-	}
+    {
+        "ivansantiagojr/jyba",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = function()
+            local jyba = require("jyba")
+            vim.keymap.set("n", "<C-m>", jyba.toggle_window)
+        end
+    }
 }
