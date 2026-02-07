a8f3c20d8c80588aaf79c0350045053a695311c3
ivansantiagojr <ivansantiago.junior@gmail.com>
2026-02-07 13:29:59 -0300

    Use zuban as python language serve

--- dotfiles/nvim/lua/config/plugins/mason.lua
+++ dotfiles/nvim/lua/config/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "zls", "ruff", "clangd", "bashls" },
+            ensure_installed = { "lua_ls", "zls", "ruff", "clangd", "bashls", "zuban" },
             automatic_installation = true,
             automatic_enable = true,
         })
