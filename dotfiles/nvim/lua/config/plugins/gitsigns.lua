101f54adaa2fcdfc6cb7eaed7d63f51cab212835
ivansantiagojr <ivansantiago.junior@gmail.com>
2026-02-15 18:22:03 -0300

    Set line blame and word diff toggles

--- dotfiles/nvim/lua/config/plugins/gitsigns.lua
+++ dotfiles/nvim/lua/config/plugins/gitsigns.lua
@@ -2,7 +2,20 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup()
+            local gitsigns = require('gitsigns')
+            gitsigns.setup({
+                on_attach = function(bufnr)
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Toggles
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+                end
+            })
         end
     }
 }
